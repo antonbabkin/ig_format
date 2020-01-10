02c07e3afe3f8d470912f7650fafd1ecd04b725c
Anton Babkin <babkin@wisc.edu>
2020-01-10 15:42:29 -0600

    List data files size

--- ig_format/_nbdev.py
+++ ig_format/_nbdev.py
@@ -2,7 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"wc_py": "00_core.ipynb",
+         "wc_sys": "00_core.ipynb",
+         "lsdir": "00_core.ipynb"}
 
 modules = ["core.py"]
 
